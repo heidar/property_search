9d07c8cc23ab6b615f8a995dafad91ec68eb2289
Heidar Bernhardsson <heidar@iseld.org>
2013-04-18 00:25:22 +0100

    Add 404 error on ActiveRecord not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  rescue_from ActiveRecord::RecordNotFound, :with => :not_found
+
+  def not_found
+    render :json => {:error => "Not found"}.to_json, :status => 404
+  end
 end
