57b6064ff8417a7cabf3e55b505cab81ac221793
Heidar Bernhardsson <heidar@iseld.org>
2013-04-21 03:48:47 +0100

    Check similar param first since it returns

--- app/models/property.rb
+++ app/models/property.rb
@@ -6,6 +6,13 @@ class Property < ActiveRecord::Base
   def self.filter_by_params params
     scoped = self.scoped
     
+    if params[:similar].present?
+      property = self.find(params[:similar])
+      properties = property.nearbys(20)
+      properties = properties.where{bedroom_count.gteq property.bedroom_count}
+      return properties
+    end
+    
     if params[:name].present?
       scoped = scoped.where{name.matches "%#{params[:name]}%"}
     end
@@ -13,13 +20,6 @@ class Property < ActiveRecord::Base
     if params[:bedroom_count].present?
       scoped = scoped.where{bedroom_count.gteq params[:bedroom_count]}
       scoped = scoped.order{bedroom_count}
-    end
-
-    if params[:similar].present?
-      property = self.find(params[:similar])
-      properties = property.nearbys(20)
-      properties = properties.where{bedroom_count.gteq property.bedroom_count}
-      return properties
     end
 
     scoped.all
