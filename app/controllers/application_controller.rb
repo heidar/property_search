bc5a2aebc12401350f2694a71b71f072ff43a651
Heidar Bernhardsson <heidar@iseld.org>
2013-04-22 05:28:25 +0100

    Add error handling for routing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActionController::RoutingError, with: :not_found
 
   def not_found
     respond_to do |format|
-      format.html { render file: 'public/404.html', status: 404 }
+      format.html { render file: 'public/404.html', status: 404, layout: false }
       format.json { render json: { error: "Not found" }.to_json, status: 404 }
     end
   end
